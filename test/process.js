1486a168447b6e9a7205ed0ce2901bbd08eb1c90
Thiago Souza <thiago@elastic.co>
2017-12-17 12:18:24 -0200

    refactor processing

---
+++ test/process.js
@@ -0,0 +1,34 @@
+/* eslint-env node, mocha */
+'use strict';
+
+import { expect } from 'chai';
+
+import process from '../lib/process';
+import * as fs from 'fs';
+
+describe('process', () => {
+
+    it('should reduce to simple array', () => 
+        new Promise((resolve, reject) => {
+            const array = [];
+            test('ndjson', '!.num[*]').
+                subscribe({
+                    next: num => array.push(num),
+                    error: err => reject(err),
+                    complete: () => resolve(array)
+                });
+        }).then(array => expect(array.sort()).to.deep.equal([
+            1, 2, 3, 4, 5, 6
+        ])));
+
+    it('should calculate max value', () =>
+        test('ndjson', '!.num[*]').
+            max().toPromise().
+            then(val => expect(val).to.equal(6)));
+
+});
+
+function test(json, path) {
+    const stream = fs.createReadStream(`${__dirname}/stream-tests/${json}.json`);
+    return process(stream, path);
+}
