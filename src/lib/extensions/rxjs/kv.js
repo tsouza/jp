c8265f11cfd804d88121e9f3859871d9c1c73499
Thiago Souza <thiago@elastic.co>
2017-12-20 15:36:41 -0200

    refactor kv operator

--- src/lib/extensions/rxjs/kv.js
+++ src/lib/extensions/rxjs/kv.js
@@ -1,10 +1,17 @@
 import { Observable } from 'rxjs';
+import _ from 'lodash';
 
-Observable.prototype.kv = function kv (elementSelector) {
+Observable.prototype.kv = function kv (keyName, elementSelector) {
+    keyName = keyName || 'key';
     elementSelector = elementSelector || ((e) => e);
     return this.
         map(value => Object.
             keys(value).
-            map(key => ({ key: key, value: elementSelector(value[key]) }))).
+            map(key => {
+                let val = elementSelector(value[key]);
+                if (!_.isObject(val))
+                    val = { value: val };
+                return _.merge({ [keyName]: key }, val);
+            })).
         flatMap(e => Observable.from(e));
 };
