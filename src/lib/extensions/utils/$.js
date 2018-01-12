77e5db5b3e9bd7d7a0f40f2d9f73152701d089b5
Thiago Souza <thiago@elastic.co>
2018-01-12 14:28:11 -0200

    fix $ util

--- src/lib/extensions/utils/$.js
+++ src/lib/extensions/utils/$.js
@@ -2,13 +2,13 @@ import jpath from 'jsonpath';
 import _ from 'lodash';
 
 export default (path, mapper = (v) => v) => object => 
-    process(path, object);
+    process(path, object, mapper);
 
-function process(path, object) {
+function process(path, object, mapper) {
     if (_.isFunction(path))
         return path(object);
     if (_.isObject(path))
         return _.mapValues(path, 
-            entry => process(entry, object));
+            entry => process(entry, object, mapper));
     return mapper(jpath.value(object, `$${path}`));
 }
