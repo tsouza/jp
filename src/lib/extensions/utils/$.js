2faf662fdefb8eb93196348b619d5c2b3ea86b9c
Thiago Souza <thiago@elastic.co>
2018-01-12 14:24:26 -0200

    support mapper in '$' util

--- src/lib/extensions/utils/$.js
+++ src/lib/extensions/utils/$.js
@@ -1,7 +1,7 @@
 import jpath from 'jsonpath';
 import _ from 'lodash';
 
-export default path => object => 
+export default (path, mapper = (v) => v) => object => 
     process(path, object);
 
 function process(path, object) {
@@ -10,5 +10,5 @@ function process(path, object) {
     if (_.isObject(path))
         return _.mapValues(path, 
             entry => process(entry, object));
-    return jpath.value(object, `$${path}`);
+    return mapper(jpath.value(object, `$${path}`));
 }
