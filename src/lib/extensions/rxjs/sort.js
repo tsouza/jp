10a7df82826bf6cb77cb6186fd8034e7e4548832
Thiago Souza <thiago@elastic.co>
2018-02-02 00:42:48 -0200

    better sorting

--- src/lib/extensions/rxjs/sort.js
+++ src/lib/extensions/rxjs/sort.js
@@ -4,13 +4,12 @@ import TinyQueue from 'tinyqueue';
 import { times } from 'lodash';
 
 Observable.prototype.sort = function sort (comparator) {
-    return this.reduce((queue, value) => {
-        queue.push(value);
-        return queue;
-    }, new TinyQueue([], comparator)).
-        map(queue => {
-            const array = [];
-            times(queue.length, () => array.push(queue.pop()));
-            return array;
-        }).flatMap(e => Observable.from(e));
+    return this.toArray().
+        map(array => new TinyQueue(array, comparator)).
+        mergeMap(queue => Observable.create(observer => {
+            for (let i = queue.length - 1; i >= 0; i--) {
+                observer.next(queue.pop());
+            }
+            observer.complete();
+        }));
 };
