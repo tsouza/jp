09bf92afb89d81c2c4cd8e2d9e26e4214002dc51
Thiago Souza <thiago@elastic.co>
2018-11-08 14:56:15 -0200

    .kv() should copy __key and __path

--- src/lib/extensions/rxjs/kv.js
+++ src/lib/extensions/rxjs/kv.js
@@ -10,7 +10,12 @@ Observable.prototype.kv = function kv (keyName = 'key', elementSelector) {
                 let val = elementSelector(value[key]);
                 if (!_.isObject(val) || _.isArray(val))
                     val = { value: val };
-                return _.merge({ [keyName]: key }, val);
+                const result = _.merge({ [keyName]: key }, val);
+                Object.defineProperties(result, {
+                    __path: value.__path,
+                    __key: value.__key        
+                });
+                return result;
             })).
         flatMap(e => Observable.from(e));
 };
