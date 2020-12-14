06835243b6d4a71d79a2c8e8118beb79900494fe
Jason Greenberg <jay.greenberg@elastic.co>
2020-12-14 06:15:07 -0500

    cleaned up build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,20 +10,12 @@ const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('clean', () => del('dist'));
 
-gulp.task('compile-js', () => merge([
-    gulp.src('src/**/*.js', { base: 'src' }).
-        pipe(babel()).
-        pipe(gulp.dest('dist')),
-    gulp.src('package.json').
-        pipe(gulp.dest('dist'))
-    ])
-);
-
-gulp.task('compile-ts', () => {
+gulp.task('compile', () => {
     let tsResult = tsProject.src().pipe(tsProject());
     return merge([
         tsResult.js.pipe(gulp.dest('dist')),
-        tsResult.dts.pipe(gulp.dest('dist'))
+        tsResult.dts.pipe(gulp.dest('dist')),
+        gulp.src('package.json').pipe(gulp.dest('dist'))
     ]);
 });
   
